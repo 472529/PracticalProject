4a48593b92468054019028a9e70e627ed2249f1d
472529 <472529@chrome.escg.ac.uk>
2021-11-23 09:28:19 +0000

    cleaning up the project

--- New Unity Project/Assets/Scripts/ThirdPersonMovement.cs
+++ New Unity Project/Assets/Scripts/ThirdPersonMovement.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public Animator animator;
     Vector3 playerVelocity;
-    public float speed = 6f;
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
+    
     public Transform cam;
+
     public bool groundedPlayer;
+
     public float maxJumps = 2;
     public float jumps;
     public float jumpHeight = 1.0f;
     public float gravityValue = -9.81f;
-    // Update is called once per frame
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+     void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        animator = GetComponentInChildren<Animator>();
+    }
     void LateUpdate()
     {
         Movement();
@@ -31,12 +40,19 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
+            
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+
+
+        if (Input.GetKey("w"))
+        {
+            animator.SetBool("isRunning", true);
         }
 
         if (groundedPlayer && playerVelocity.y < 0)
